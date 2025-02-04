ee399cb00209c3cc5ce42898f5bc83c8933708a8
NaveenDev-Hub <naveensct@gmail.com>
2025-02-03 22:35:08 -0600

    Time: 32 ms (32.14%), Space: 25.8 MB (33.4%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -6,9 +6,9 @@ class Solution:
         left_arr = [0] * n
         right_arr = [0] * n
         for i in range(n):
-            j = -i -1
+            j = -i - 1
             left_arr[i] = left_mul
             right_arr[j] = right_mul
             left_mul *= nums[i]
             right_mul *= nums[j]
-        return [l*r for l,r in zip(left_arr, right_arr)]
+        return [l * r for l, r in zip(left_arr, right_arr)]

811805ea0d9688bcc5def6f5944e406b075d65e0
NaveenDev-Hub <naveensct@gmail.com>
2025-03-14 21:35:38 -0500

    Time: 0 ms (100%), Space: 17.6 MB (88.01%) - LeetHub

--- 2083-three-divisors/2083-three-divisors.py
+++ 2083-three-divisors/2083-three-divisors.py
@@ -4,12 +4,10 @@ class Solution:
             return False
 
         root = int(n ** 0.5)
-        print("root: ", root)
         if n != int(root * root):
             return False
 
         #find if root is prime
-        print("summma: ", int(root ** 0.5))
         for i in range(2, int(root ** 0.5) + 1):
             if root % i == 0:
                 return False
